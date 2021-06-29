1c5a9a805600e73fd31e06a0b8116edab6ed90ce
FelipeNFL <felipe.nunes@finpass.com.br>
2021-06-29 04:03:41 -0300

    Create tests to Matching API

--- test/matching_api_test.go
+++ test/matching_api_test.go
@@ -1,24 +1,85 @@
 package test
 
 import (
+	"os"
 	"testing"
+	"net/http"
+	"yawoen/core"
 )
 
-func TestGetCompanySuccessful(t *testing.T) {
+const HOST = "http://localhost:8081"
 
+func createRow(row core.Company) {
+	db, _ := core.GetDb()
+	db.Create(&row)
+}
+
+func setup() {
+	core.CleanDb()
+	core.InitializeDb()
+	createRow(core.Company{Name: "TEST FULLNAME", Zipcode: "12345"})	
+}
+
+func shutdown() {
+	core.CleanDb()
+}
+
+func TestMain(m *testing.M) {
+    setup()
+    code := m.Run() 
+    shutdown()
+    os.Exit(code)
+}
+
+func TestGetCompanySuccessfulFullname(t *testing.T) {
+	url := HOST + "/company?name=Test%20Fullname&zipcode=12345"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("status code wrong")
+	}
+}
+
+func TestGetCompanySuccessfulPartialName(t *testing.T) {
+	url := HOST + "/company?name=Test&zipcode=12345"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("status code wrong")
+	}
 }
 
 func TestGetCompanyNotFound(t *testing.T) {
+	url := HOST + "/company?name=Wrong&zipcode=123"
+	resp, err := http.Get(url)
 
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if resp.StatusCode != 404 {
+		t.Errorf("status code wrong")
+	}
 }
 
 func TestHealthMatchingApi(t *testing.T) {
-	// const URL = fmt.Sprintf(HOST, "/health")
+	url := HOST + "/health"
+	resp, err := http.Get(url)
 
-	// resp, err := http.Get(URL)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
 
-	// if err != nil {
-	// 	t.Errorf(err)
-	// }
+	if resp.StatusCode != 200 {
+		t.Errorf("status code wrong")
+	}
 }
 
